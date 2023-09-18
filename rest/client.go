1017b8a23b7cccdc8c1e0024b10d77346610ea74
Peterson <petersonchery33@gmail.com>
2023-09-18 11:13:59 -0700

    Library Project

--- rest/client.go
+++ rest/client.go
@@ -1,11 +1,10 @@
 package rest
 
-	//gestionnaire de route
-	func GestRoute(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("Welcome Peet"))
-	}
-	
-	func GestRoute1(w http.ResponseWriter, r *http.Request){
-		w.Write ([]byte("I am here"))
-	}
-	
+// //gestionnaire de route
+// func GestRoute(w http.ResponseWriter, r *http.Request){
+// 	w.Write([]byte("Welcome Peet"))
+// }
+
+// func GestRoute1(w http.ResponseWriter, r *http.Request){
+// 	w.Write ([]byte("I am here"))
+// }
